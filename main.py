38e99137f87ec47de0ddbcdd6f6ece97f3578153
Iuleoo <KlyinT@qq.com>
2024-01-27 13:23:41 +0800

    数据库操作封装

--- main.py
+++ main.py
@@ -36,10 +36,8 @@ captchaVerification = login.encryptAesEcb(tokenCoordinateBytes, secretKeyBytes)
 login.checkVerification(encryptedVerification, token.decode('utf-8'))
 
 # 登录
-phone = 18385092934
-passwd = "01090011"
-# login.login(phone, passwd, captchaVerification)
-# data = login.login(phone, passwd, captchaVerification)
+phone = 17388542263
+passwd = "06180136"
 login_data = login.login(phone, passwd, captchaVerification)
 
 # 将得到的登录信息保存为JSON
@@ -49,23 +47,6 @@ with open('login_data.json', 'w') as json_file:
 # 重新加载这些JSON数据
 with open('login_data.json', 'r') as json_file:
     login_data_loaded = json.load(json_file)
-
-# 打印已保存的JSON数据
-# print(login_data_loaded)
-# print(login_data_loaded['data']['userId'])
-# print(login_data_loaded['data']['userName'])
-# print(login_data_loaded['data']['phonenumber'])
-# print(login_data_loaded['data']['sexName'])
-# print(login_data_loaded['data']['schoolName'])
-# print(login_data_loaded['data']['collegeName'])
-# print(login_data_loaded['data']['majorName'])
-# print(login_data_loaded['data']['className'])
-# print(login_data_loaded['data']['teacherName'])
-# print(login_data_loaded['data']['enterpriseId'])
-# print(login_data_loaded['data']['enterpriseName'])
-# print(login_data_loaded['data']['studentCode'])
-# print(login_data_loaded['data']['token'])
-# print(login_data_loaded['data']['expTime'])
 
 # 连接数据库
 connection = database.connect_to_database()
@@ -88,22 +69,19 @@ studentCode = (login_data_loaded['data']['studentCode'])
 token = (login_data_loaded['data']['token'])
 expTime = (login_data_loaded['data']['expTime'])
 
-# 查询用户是否存在
+# 检查用户是否存在于 user 表中
 sql = f"SELECT * FROM user WHERE userId = '{userId}'"
 cursor.execute(sql)
 result = cursor.fetchall()
 
-# 如果用户存在，则更新 token
+# 如果用户存在，则更新 user 表中的 token
 if result:
     user_sql = f"""
         UPDATE user
-        SET
-            token = '{token}'
-        WHERE
-            userId = '{userId}'
+        SET token = '{token}'
+        WHERE userId = '{userId}'
     """
-
-# 如果用户不存在，则插入数据
+# 如果用户不存在，则将用户数据插入 user 表中
 else:
     user_sql = f"""
     INSERT INTO user (
@@ -140,27 +118,19 @@ else:
     );
     """
 
-# 打印SQL语句
-# print(user_sql)
-
-
-# user_info 表
-
-# 查询用户是否存在
+# 检查用户是否存在于 user_info 表中
 sql = f"SELECT * FROM user_info WHERE userId = '{userId}'"
 cursor.execute(sql)
 result = cursor.fetchall()
 
-# 如果用户存在，则更新 phonenumber
+# 如果用户存在，则更新 user_info 表中的 phonenumber
 if result:
     user_info_sql = f"""
         UPDATE user_info
-        SET
-            phonenumber = '{phonenumber}'
-        WHERE
-            userId = '{userId}'
+        SET phonenumber = '{phonenumber}'
+        WHERE userId = '{userId}'
     """
-# 如果用户不存在，则插入数据
+# 如果用户不存在，则将用户数据插入 user_info 表中
 else:
     user_info_sql = f"""
     INSERT INTO user_info (userId, userName, phonenumber)
@@ -171,6 +141,7 @@ else:
     );
     """
 
+# 执行 SQL 语句
 cursor.execute(user_sql)
 cursor.execute(user_info_sql)
 
