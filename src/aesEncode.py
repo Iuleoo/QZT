8efca95bef07d27bb27c404bd33c4e847de37a42
Iuleoo <KlyinT@qq.com>
2024-01-31 20:37:06 +0800

    解决 pycryptodome 库与 Python 版本不兼容导致的

--- src/aesEncode.py
+++ src/aesEncode.py
@@ -1,3 +1,8 @@
+# 报错 SystemError: PY_SSIZE_T_CLEAN macro must be defined for '#' formats
+# 这个问题可能是由于 pycryptodome 库与 Python 版本不兼容导致的。
+# 此问题出现在Docker容器中，使用 Python 3.11.7 版本会出现，以上情况安装 pycryptodomex 库，改为 from Cryptodome.Cipher import AES
+# from Cryptodome.Cipher import AES
+
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import base64
